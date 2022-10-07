1eea48485f9119c7f166054fd292e9684081ba47
Thamy <>
2022-10-07 19:59:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+#questão 1
+for x in range(1,6):
+  print('Você é maravilindo')
+print("\n")
+  
+#questão 2  
+for pedido in range(1,11):
+  nome_usuario = input("Nome do usuário:")
+  print(nome_usuario, "eu amo programação.") 
+print("\n")
+
+#questão 3
+for x in range(10,41):
+  nome_irmao = input("Seu nome:")
+  print(nome_irmao, "o número é", x)
+print("\n")
